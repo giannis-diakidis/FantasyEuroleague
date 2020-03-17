c0b4752f657c40d136528f26d5fb822abd1b7a06
idwebdesign <idiakidis@hotmail.com>
2020-03-17 12:20:16 +0200

    AppManager as the only Role

--- FantasyEuroleague/Models/RoleName.cs
+++ FantasyEuroleague/Models/RoleName.cs
@@ -8,7 +8,5 @@ namespace FantasyEuroleague.Models
     public static class RoleName
     {
         public const string AppManager = "AppManager";
-        public const string Player1 = "Player1";
-        public const string Player2 = "Player2";
     }
 }
